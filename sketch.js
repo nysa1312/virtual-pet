e3a3535968b947632024cf36b075855d8b2f0c62
nysa1312 <86355089+nysa1312@users.noreply.github.com>
2021-10-06 17:48:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var dog = ("dog1", 'dogImg.png');
+var happyDog = ("dog2", 'dogImg1.png');
+var database , foodS , foodStock;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
